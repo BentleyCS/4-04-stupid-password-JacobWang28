e5b5883960843f2c58b4fb00a843df3716499f92
JacobWang28 <jwang28@bentleyschool.org>
2026-01-05 22:58:53 -0800

    Add files via upload

--- test_CSP_4_04_Stupid_Password_generator.py
+++ test_CSP_4_04_Stupid_Password_generator.py
@@ -1,4 +1,16 @@
 import CSP_4_04_Stupid_Password_generator as HW
-def test_stupid_password():
-    assert HW.stupidPassword(2,4)==["11aa2", "11ab2", "11ac2", "11ad2", "11ba2", "11bb2", "11bc2", "11bd2", "11ca2", "11cb2", "11cc2", "11cd2", "11da2", "11db2", "11dc2", "11dd2"]
-    assert HW.stupidPassword(3,1) == ["11aa2", "11aa3", "12aa3", "21aa3", "22aa3"]
+def stupid_password(n,l):
+    password=[]
+    letters="abcdefghijklmnopqrstuvwxyz"
+    for d1 in range(1, n+1):
+        for d2 in range(1, n+1):
+            for d3 in letters[:l]:
+                for d4 in letters[:l]:
+                    for d5 in range(1,n+1):
+                        if d5>d1 and d5>d2:
+                            password.append(str(d1)+str(d2)+d3+d4+str(d5))
+    return password
+print (stupid_password(2,2))
+
+
+
